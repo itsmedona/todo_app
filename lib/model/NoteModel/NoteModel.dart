2b85796c8c359cd8b0aaa9687a35abf84fbeb2a1
itsmedona <donashaji254@gmail.com>
2023-11-15 15:47:11 +0530

    initial commit

---
+++ lib/model/NoteModel/NoteModel.dart
@@ -0,0 +1,6 @@
+class NoteModel {
+  NoteModel({required this.title, this.date, required this.description});
+  String title;
+  String? date;
+  String description;
+}
---
+++ lib/view/HomeScreen/HomeScreen_Widget/HomeScreen_Widget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenWidget extends StatelessWidget {
+ const  HomeScreenWidget({super.key, required this.title, required this.description, required this.date, this.onDeletetap});
+ final String title;
+ final String description;
+ final String? date; 
+ final VoidCallback? onDeletetap;//or ontap nte type copy chyth ida
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+              shape:RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20)
+                   
+              ),
+              
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(title,style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold),),
+                        Row(
+                          children: [
+                           
+                             InkWell(
+                              onTap:onDeletetap ,
+                              child: Icon(Icons.delete)),
+                            SizedBox(width: 20,),
+                            Icon(Icons.edit)
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(description,style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    
+                    
+                    children: [
+                      Positioned(
+                        top: 0,
+                        bottom: 10,
+                        right: 20,
+                        child: Text("",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)),
+                    ],
+                  )
+                  
+                
+                ],
+              ),
+              color: Color.fromARGB(255, 154, 248, 201),             
+);
+}
+}
