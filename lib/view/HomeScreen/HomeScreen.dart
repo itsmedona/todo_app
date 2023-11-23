dbfc3ccfd9bf2ebbb7f26ff5d132b25838f20ab9
itsmedona <donashaji254@gmail.com>
2023-11-23 23:30:53 +0530

    modified

--- lib/view/HomeScreen/HomeScreen.dart
+++ lib/view/HomeScreen/HomeScreen.dart
@@ -5,151 +5,189 @@ import 'package:to_do_app/view/HomeScreen/HomeScreen_Widget/HomeScreen_Widget.da
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
 
-
-
   @override
   State<Homescreen> createState() => _HomescreenState();
 }
 
 class _HomescreenState extends State<Homescreen> {
-  final nameController =TextEditingController();
-  final desController=TextEditingController();
-  final dateController=TextEditingController();
-  List<NoteModel>myNoteList=[ /////seperate model class
-    NoteModel(title: "About My day", date: "jan2", description: "Not So Good"),
-     
+  final nameController = TextEditingController();
+  final desController = TextEditingController();
+  final dateController = TextEditingController();
+  List<NoteModel> myNoteList = [
+    /////seperate model class
+    NoteModel(title: " ", date: " ", description: "  "),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text("ToDo"),
+        title: Text("ToDo"),
         backgroundColor: Color.fromARGB(255, 8, 227, 125),
-         ),
-       body: Padding(
-         padding: const EdgeInsets.all(20),
-         child: ListView.builder(
-            itemCount:myNoteList.length ,
-            itemBuilder: (context, index) => SizedBox(
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: ListView.builder(
+          itemCount: myNoteList.length,
+          itemBuilder: (context, index) => SizedBox(
               height: 200,
               width: 300,
-              child: HomeScreenWidget(//created widget
-              onDeletetap: () {
-                myNoteList.removeAt(index);//for deleting it from the widget declaration and here
-                setState(() {
-                  
-                });
-              },
-                title:myNoteList[index].title,
+              child: HomeScreenWidget(
+                //created widget
+                onDeletetap: () {
+                  myNoteList.removeAt(
+                      index); //for deleting it from the widget declaration and here
+                  setState(() {});
+                },
+                title: myNoteList[index].title,
                 description: myNoteList[index].description,
                 date: myNoteList[index].date,
-              )
-            ),
-            ),
-       ),
-       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          showModalBottomSheet(context: context, builder:(context) => Container(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration:BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),color:Color(0xffEBE3D5)
-                      ) ,
-                      child: TextField(
-                        controller: nameController,
-                        
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          
-                          hintText: "Title"
+              )),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) => Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Color(0xffEBE3D5)),
+                        child: TextField(
+                          controller: nameController,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(), hintText: "Title"),
                         ),
-                        
                       ),
-                      
                     ),
-                  ),
-                  SizedBox(width: 20,),
-                  Container(
-                  padding: EdgeInsets.symmetric(horizontal: 50,vertical: 50),
-                    decoration:BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),color:Color(0xffEBE3D5)
-                      ) ,
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 50, vertical: 50),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Color(0xffEBE3D5)),
                       child: TextField(
                         controller: desController,
-                        
                         decoration: InputDecoration(
-                          border: InputBorder.none,
-                          
-                          hintText: "Description"
-                        ),
-                        
+                            border: InputBorder.none, hintText: "Description"),
                       ),
-            
-                  ),
-
-                  SizedBox(height: 20,),
-                  Container(
-                  
-                    decoration:BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),color:Color(0xffEBE3D5)
-                      ) ,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Color(0xffEBE3D5)),
                       child: TextField(
                         controller: dateController,
-                        
                         decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          
-                          hintText: "date"
-                        ),
-                        
+                            border: OutlineInputBorder(), hintText: "date"),
                       ),
-            
-                  ),
-                  SizedBox(height: 10,),
-                  Row(
-                    children: [
-                      InkWell(child: Container(height: 50,width: 50,decoration: BoxDecoration(color: Color.fromARGB(255, 107, 230, 111),borderRadius: BorderRadius.circular(20)),)),
-                      SizedBox(width: 15,),
-                       InkWell(child: Container(height: 50,width: 50,decoration: BoxDecoration(color: Color.fromARGB(255, 243, 173, 62),borderRadius: BorderRadius.circular(20)),)),
-                       SizedBox(width: 15,),
-                        InkWell(child: Container(height: 50,width: 50,decoration: BoxDecoration(color: Color.fromARGB(255, 123, 218, 237),borderRadius: BorderRadius.circular(20)),)),
-                         SizedBox(width: 15,),
-                        InkWell(child: Container(height: 50,width: 50,decoration: BoxDecoration(color: Color.fromARGB(255, 236, 146, 202),borderRadius: BorderRadius.circular(20)),)),
-                         SizedBox(width: 15,),
-                        InkWell(child: Container(height: 50,width: 50,decoration: BoxDecoration(color: Color.fromARGB(255, 240, 96, 96),borderRadius: BorderRadius.circular(20)),)),
-                         SizedBox(width: 15,),
-                        InkWell(child: Container(height: 50,width: 50,decoration: BoxDecoration(color: Color.fromARGB(255, 172, 76, 227),borderRadius: BorderRadius.circular(20)),)),
-                        
-                    ],
-                  ),
-                  SizedBox(height: 8,),
-                 ElevatedButton(onPressed: (){
-                 myNoteList.add(
-                  NoteModel(
-                    title:nameController.text , date:dateController.text , description: desController.text
-                    )
-                 );
-                 setState(() {
-                   
-                 });
-                 nameController.clear();
-                 desController.clear();
-                 dateController.clear();   
-                 Navigator.pop(context);             
-                 },
-                  child: Text("Save"))
-                ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        InkWell(
+                            child: Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 107, 230, 111),
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        InkWell(
+                            child: Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 243, 173, 62),
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        InkWell(
+                            child: Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 123, 218, 237),
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        InkWell(
+                            child: Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 236, 146, 202),
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        InkWell(
+                            child: Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 240, 96, 96),
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        InkWell(
+                            child: Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 172, 76, 227),
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          myNoteList.add(NoteModel(
+                              title: nameController.text,
+                              date: dateController.text,
+                              description: desController.text));
+                          setState(() {});
+                          nameController.clear();
+                          desController.clear();
+                          dateController.clear();
+                          Navigator.pop(context);
+                        },
+                        child: Text("Save"))
+                  ],
+                ),
               ),
             ),
-          ),
-           );
-        },child: Icon(Icons.add),),
-        );
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
-
